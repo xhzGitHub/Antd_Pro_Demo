60ddd53635d447cafe3759de6d4fbf81f687dee5
肖鸿泽 <xiaohongze@xiaohongzedeMacBook-Pro.local>
2019-07-23 12:08:26 +0800

    0723 semi

--- src/pages/Subject/SubjectRegion.tsx
+++ src/pages/Subject/SubjectRegion.tsx
@@ -1,104 +1,104 @@
-import React, { useReducer, useEffect, useCallback, Fragment } from 'react'
-import { Card, Form, Input, Upload, Icon, Button, Skeleton } from 'antd'
-import { IntlProvider } from 'react-intl'
-import { RouterChildProps } from '@/types/router'
-import { getSubjectRegionDetail } from '@/services/subject'
-import { debounce } from 'lodash'
+import React, { useReducer, useEffect, useCallback, Fragment } from "react";
+import { Card, Form, Input, Upload, Icon, Button, Skeleton } from "antd";
+import { IntlProvider } from "react-intl";
+import { RouterChildProps } from "@/types/router";
+import { getSubjectRegionDetail } from "@/services/subject";
+import { debounce } from "lodash";
 
 interface Region {
-  name: string
-  image: string
-  description: string
+  name: string;
+  image: string;
+  description: string;
 }
 
 interface State {
-  formData: Region
-  isLoading: boolean
+  formData: Region;
+  isLoading: boolean;
 }
 
 type Action =
-  | { type: 'INIT'; formData: Region; isLoading: boolean }
-  | { type: 'SET_REGION_NAME'; name: Region['name'] }
+  | { type: "INIT"; formData: Region; isLoading: boolean }
+  | { type: "SET_REGION_NAME"; name: Region["name"] };
 
 const reducer = (state: State, action: Action) => {
   switch (action.type) {
-    case 'INIT':
+    case "INIT":
       return {
         ...state,
         formData: {
           ...state.formData,
-          ...action.formData,
+          ...action.formData
         },
-        isLoading: action.isLoading,
-      }
-    case 'SET_REGION_NAME':
+        isLoading: action.isLoading
+      };
+    case "SET_REGION_NAME":
       return {
         ...state,
         formData: {
           ...state.formData,
-          name: action.name,
-        },
-      }
+          name: action.name
+        }
+      };
   }
-}
+};
 
-const { Dragger } = Upload
+const { Dragger } = Upload;
 
 const formItemLayout = {
   labelCol: { span: 6 },
-  wrapperCol: { span: 14 },
-}
+  wrapperCol: { span: 14 }
+};
 
 const initialState = {
   formData: {
-    name: '',
-    image: '',
-    description: '',
+    name: "",
+    image: "",
+    description: ""
   } as Region,
-  isLoading: false,
-}
+  isLoading: false
+};
 
 function SubjectRegion(props: RouterChildProps) {
-  const regionId = props.match.params.id
-  const isCreating = regionId === undefined
+  const regionId = props.match.params.id;
+  const isCreating = regionId === undefined;
   const [state, dispatch] = useReducer(reducer, initialState, state =>
     isCreating ? state : { ...state, isLoading: true }
-  )
+  );
 
   useEffect(() => {
     if (!isCreating) {
-      ;(async () => {
-        const res = await getSubjectRegionDetail(regionId)
+      (async () => {
+        const res = await getSubjectRegionDetail(regionId);
         if (Object.keys(res).length) {
-          const { name, image, description } = res
+          const { name, image, description } = res;
           const formData = {
             name,
             image,
-            description,
-          }
-          dispatch({ type: 'INIT', formData, isLoading: false })
+            description
+          };
+          dispatch({ type: "INIT", formData, isLoading: false });
         }
-      })()
+      })();
     }
-  }, [])
+  }, []);
 
   // useCallback 可再考虑一下！
   const handleSetRegionName = useCallback(
     debounce(val => {
       dispatch({
-        type: 'SET_REGION_NAME',
-        name: val,
-      })
+        type: "SET_REGION_NAME",
+        name: val
+      });
     }, 500),
     [state]
-  )
+  );
 
   const handleSetRegionImage = ({ fileList }) => {
     // TODO
-    console.log('')
-  }
+    console.log("");
+  };
 
-  const { name, image, description } = state.formData
+  const { name, image, description } = state.formData;
 
   return (
     <IntlProvider>
@@ -112,7 +112,7 @@ function SubjectRegion(props: RouterChildProps) {
                 <Input
                   placeholder="输入商圈名称"
                   onChange={e => {
-                    handleSetRegionName(e.target.value)
+                    handleSetRegionName(e.target.value);
                   }}
                 />
               ) : (
@@ -129,11 +129,11 @@ function SubjectRegion(props: RouterChildProps) {
                 </Dragger>
               ) : (
                 <Card
-                  cover={<img src={image} style={{ width: '300px', height: '300px' }} />}
+                  cover={<img src={image} style={{ width: "300px", height: "300px" }} />}
                   actions={[
                     <Button type="danger">
                       <Icon type="delete" />
-                    </Button>,
+                    </Button>
                   ]}
                 />
               )}
@@ -153,8 +153,8 @@ function SubjectRegion(props: RouterChildProps) {
               </Fragment>
             )}
             <Form.Item wrapperCol={{ offset: 6 }}>
-              <Button type="primary" style={{ marginRight: '10px' }}>
-                {isCreating ? '创建' : '保存'}
+              <Button type="primary" style={{ marginRight: "10px" }}>
+                {isCreating ? "创建" : "保存"}
               </Button>
               <Button type="primary">取消</Button>
             </Form.Item>
@@ -162,7 +162,7 @@ function SubjectRegion(props: RouterChildProps) {
         )}
       </Card>
     </IntlProvider>
-  )
+  );
 }
 
-export default Form.create()(SubjectRegion)
+export default Form.create()(SubjectRegion);
